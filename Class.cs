893e33a4d6ca09ca02b562bc75e891b374dcf071
Tyler M <16845057+TylerDM@users.noreply.github.com>
2020-03-04 08:28:04 -0500

    Added default class.cs.

---
+++ Class.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace $rootnamespace$
+{
+    public class $safeitemrootname$
+    {
+        #region fields
+        #endregion
+
+        #region properties
+        #endregion
+
+        #region constructors
+        #endregion
+
+        #region methods
+        #endregion
+
+        #region private methods
+        #endregion
+    }
+}
